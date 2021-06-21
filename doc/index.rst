a6663c6f590bdea5bc8edcd2e3a30f9b7e0bf0be
Michael Christensen <amichaelc@protonmail.ch>
2021-06-21 09:36:53 -0600

    Change format of command and style of code

--- doc/index.rst
+++ doc/index.rst
@@ -1,26 +1,22 @@
 ansi-colors
-===============================================================================
+===========
 
 Synopsis
--------------------------------------------------------------------------------
+--------
 
-**ansi-colors** [*upper* | *lower* *upper*]
+**ansi-colors** [*lower* *upper*]
 
 Description
--------------------------------------------------------------------------------
+-----------
 
 Print numbers from 0 to 150, and format using ANSI escape sequences
 (specifically **\\e[m**).
-
-*upper*
-
-    Print numbers from 0 to *upper*
 
 *lower* *upper*
 
     Print numbers from *lower* to *upper*
 
 See also
--------------------------------------------------------------------------------
+--------
 
 More projects at https://github.com/amichaelc
--- src/ansi-colors.c
+++ src/ansi-colors.c
@@ -1,36 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    int upper = 150;
-    int lower = 0;
-    int i;
+int main(int argc, char *argv[])
+{
+	int upper = 150;
+	int lower = 0;
+	int i;
 
-    if (argc < 4) {
-        if (argc == 2) {
-            upper = atoi(argv[1]);
-        }
-
-        if (argc == 3) {
-            lower = atoi(argv[1]);
-            upper = atoi(argv[2]);
-
-            for (i = lower; (i % 10) > 0; i--) {
-                printf("    ");
-            }
-        }
-
-        for (i = lower; i < upper; i++) {
-            printf("\e[%dm%3d\e[m ", i, i);
-
-            if (i % 10 == 9) {
-                printf("\n");
-            }
-        }
-    } else {
-        fprintf(stderr, "Expected 0, 1, or 2 args, got %d\n", argc - 1);
-        return 1;
-    }
-
-    return 0;
+	switch (argc) {
+	case 3:
+		lower = atoi(argv[1]);
+		upper = atoi(argv[2]);
+		for (i = lower; (i%10) > 0; i--)
+			printf("    ");
+		/* fall through */
+	case 1:
+		for (i = lower; i < upper; i++) {
+			printf("\e[%dm%3d\e[m ", i, i);
+			if (i%10 == 9)
+				printf("\n");
+		}
+		return 0;
+	default:
+		fprintf(stderr, "Expected 0 or 2 args, got %d\n", argc-1);
+		return 1;
+	}
 }
